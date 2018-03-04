6821314a24b4b7bf48cb93a1e748f18b0ff304d1
System Administrator <root@Bilals-iMac.local>
2018-03-04 23:01:19 +0500

    udate

--- index.php
+++ index.php
@@ -565,6 +565,7 @@ can be dyed to any color to fit into color scheme of the season.</p>
                     <div class="col section-heading mt-5 mt-md-1 mt-lg-5 mb-5">
                             <h1>Our Market</h1>
                     </div>
+                    <iframe src="https://snazzymaps.com/embed/52596" width="100%" height="400px" style="border:none;" /></iframe>
             </section>
 
             <section id="Contact" class="jumbotron">
