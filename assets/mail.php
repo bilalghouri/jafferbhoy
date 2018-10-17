642896d1e88595e9f7ac45839df3c8442125151c
bilalghouri <bilal@linksnappy.com>
2018-10-17 18:20:37 +0500

    fixes

--- assets/mail.php
+++ assets/mail.php
@@ -27,6 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         }
         //Recipients
         $mail->setFrom("no-reply@jafferbhoymeheralli.com", "Contact Form");
+        $mail->addAddress("bilalghouri@gmail.com");
         $mail->addAddress("Aly@jafferbhoymeheralli.com");
         $mail->addCC('Jafferm64@hotmail.com');
         $mail->addCC('bilal@obsidianmedia.io');
