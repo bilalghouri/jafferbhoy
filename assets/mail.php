8c6c0a7609061cbcec88b724c82538f3ec403b03
bilalghouri <bilal@linksnappy.com>
2018-10-17 17:24:09 +0500

    fixes

--- assets/mail.php
+++ assets/mail.php
@@ -8,7 +8,6 @@ require 'PHPMailer/src/PHPMailer.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-
     $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
     try {
         $firstname = strip_tags(trim($_POST["firstname"]));
@@ -21,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($firstname) OR empty($lastname) OR empty($company) OR empty($subject) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             http_response_code(400);
             throw new Exception("Please complete the form and try again.");
         }
@@ -60,44 +59,4 @@ else
     http_response_code(403);
     echo "There was a problem with your submission, please try again.";
 }
-
-
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-
-
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "info@yoursite.com";
-
-        // Set the email subject.
-        $subject = "New contact from $name";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n\n";
-        $email_content .= "Subject: $subject\n\n";
-        $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Thank You! Your message has been sent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
-        }
-
-    } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
-    }
-
 ?>
